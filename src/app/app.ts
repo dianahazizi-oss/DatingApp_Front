dab5a38c15cbf10c5dcb5c3210b4ccd1dd2679b2
Diana Hazizi <dhazizi@helius-systems.com>
2026-06-01 12:15:25 +0200

    first commit angular - course section 3

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('DatingAppFront');
+export class App implements OnInit{
+
+  private http = inject(HttpClient);
+  protected title = 'Dating App';
+
+  protected members = signal<any>([])
+
+    async ngOnInit() {
+     this.members.set(await this.getMembers())
+  }
+
+  async getMembers(){
+    try{
+          return lastValueFrom (this.http.get('https://localhost:5001/api/members'));
+    }
+    catch(error){
+        console.log(error);
+        throw error;
+    }
+  }
+
 }
